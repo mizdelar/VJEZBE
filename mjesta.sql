695521e1e3e2219c45763b1e38513aee5a900f94
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-22 15:40:49 +0200

    OBŽmjesta

--- mjesta.sql
+++ mjesta.sql
@@ -914,3 +914,6 @@ INSERT INTO  mjesto  ( postanskibroj ,  zupanija ,  naziv ) VALUES
 ('53291', 9, 'Novalja'),
 ('53294', 9, 'Lun'),
 ('53296', 9, 'Zubovići');
+
+
+select * from mjesto where postanskibroj like '31%';
